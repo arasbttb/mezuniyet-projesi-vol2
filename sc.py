665b8a6d52745d0cea9d3304b7802e73dcd7ff86
arasbttb <einstenokul@gmail.com>
2026-01-17 22:09:04 +0300

    Update sc.py

--- sc.py
+++ sc.py
@@ -13,6 +13,8 @@ s5 = {"teknik destekle nasıl iletişime geçebilirim?", "teknik destekle nasıl
 
 s6 = {"ödeme sırasında teslimat yöntemini değiştirebilir miyim?", "ödeme sırasında teslimat yöntemini değiştirebilir miyim", "teslimat değişikliği"}
 
+s7 =  {"iade edebilir miyim", "teknik olarak ürünü değiştirebilr miyim", "ürünü fiyat iadesi alabilir miyim"}
+
 c1 = "Alışveriş yapmak için, ilgilendiğiniz ürünü seçip -Alışveriş Sepetine Ekle- butonuna tıklayın. Ardından Alışveriş Sepetine gidin ve satın alma işlemini tamamlamak için yönergeleri takip edin."
 
 c2 = "Siparişinizin durumunu öğrenmek için internet sitemizdeki hesabınıza giriş yapın ve -Siparişlerim- bölümüne gidin. Orada, siparişinizin mevcut durumunu görebilirsiniz."
@@ -24,3 +26,5 @@ c4 = "Hasarlı bir ürün aldıysanız, lütfen hemen müşteri hizmetlerimizle 
 c5 = "Teknik destekle, internet sitemizde yer alan telefon numarasını arayarak iletişime geçebilirsiniz. Alternatif olarak, sohbet robotumuz üzerinden de bizimle iletişim kurabilirsiniz."
 
 c6 = "Evet, ödeme sayfasında teslimat bilgilerini değiştirebilirsiniz. Kullanılabilir teslimat yöntemleri ve şartları orada listelenecektir."
+
+c7 = "Evet, ürünlerimizi satın aldıktan sonra 15 gün içinde iade edebilirsiniz. İade politikamız ve prosedürlerimiz hakkında daha fazla bilgi için lütfen internet sitemizdeki -İade Politikası- sayfasını ziyaret edin."
